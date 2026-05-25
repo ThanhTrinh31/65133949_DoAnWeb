fb7408a786c9b13c645aa1dabf19844337015658
ThanhTrinh31 <Huynhthanhtrinh2812@gmail.com>
2026-05-25 19:53:41 +0700

    dong code dau tien

--- seafood_store/src/main/java/__HuynhThanhTrinh/HaiSan.java
+++ seafood_store/src/main/java/__HuynhThanhTrinh/HaiSan.java
@@ -1,8 +1,9 @@
+package __HuynhThanhTrinh;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import lombok.Data;
 
-@Data
+
 @Document(collection = "san_pham")
 public class HaiSan {
     @Id
@@ -11,4 +12,29 @@ public class HaiSan {
     private String ten;
     private double giaBan;
     private int soLuong;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTen() {
+		return ten;
+	}
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+	public double getGiaBan() {
+		return giaBan;
+	}
+	public void setGiaBan(double giaBan) {
+		this.giaBan = giaBan;
+	}
+	public int getSoLuong() {
+		return soLuong;
+	}
+	public void setSoLuong(int soLuong) {
+		this.soLuong = soLuong;
+	}
+    
 }
