cb25c0819ebe1d9d4aaa10a895d0127d4db03068
ThanhTrinh31 <Huynhthanhtrinh2812@gmail.com>
2026-05-29 06:33:13 +0700

    go go

--- seafood-store/seafood-store/src/main/java/com/huynhthanhtrinh/seafood_store/HaiSanController.java
+++ seafood-store/seafood-store/src/main/java/com/huynhthanhtrinh/seafood_store/HaiSanController.java
@@ -1,11 +1,13 @@
 package com.huynhthanhtrinh.seafood_store;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class HaiSanController {
 
     @Autowired
@@ -14,19 +16,6 @@ public class HaiSanController {
     // Đường dẫn API để lấy danh sách
     @GetMapping("/api/haisan")
     public List<HaiSan> layDanhSachHaiSan() {
-        List<HaiSan> danhSach = repository.findAll();
-        
-        // Nếu kho rỗng, ép Spring Boot tự động thêm 1 món vào kho để test
-        if (danhSach.isEmpty()) {
-            HaiSan hs = new HaiSan();
-            hs.setTen("Cua Hoàng Đế (Auto Test)");
-            hs.setGiaBan(2000000.0);
-            hs.setSoLuong(5);
-            repository.save(hs); // Lưu thẳng vào Database
-            
-            danhSach = repository.findAll(); // Cập nhật lại danh sách để hiển thị
-        }
-        
-        return danhSach; 
+        return repository.findAll(); 
     }
 }
